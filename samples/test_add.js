c540ba82fbde7ba98247e03a01e215234b666fda
rexim <reximkut@gmail.com>
2020-10-15 18:17:23 +0700

    Add add_u8 sample

--- samples/test_add.js
+++ samples/test_add.js
@@ -7,6 +7,7 @@ const assert = require("assert");
 WebAssembly
     .instantiate(fs.readFileSync(`${__dirname}/add.wasm`))
     .then((wasm) => {
+        assert.equal(wasm.instance.exports.add_u8(10, 20), 30);
         assert.equal(wasm.instance.exports.add_u32(10, 20), 30);
         assert.equal(wasm.instance.exports.add_u64(10n, 20n), 30n);
         console.log("test_add ok");
